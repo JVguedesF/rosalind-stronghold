e0f2797722765e2d97bca27186d006e3d5f6db40
João Vitor Guedes <jvfernandesg4@outlook.com>
2026-01-08 20:42:20 -0300

    feat(utils): add FASTA file parsing utility

--- utils/utils.py
+++ utils/utils.py
@@ -12,6 +12,32 @@ def read_input(filename="input.txt"):
     path = _get_path(filename)
     with open(path, "r") as f:
         return f.read().strip()
+    
+def read_fasta(filename="input.txt"):
+    path = _get_path(filename) 
+    sequences = {}
+    current_id = None
+    current_sequence = []
+    
+    with open(path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line: continue
+            
+            if line.startswith(">"):
+                if current_id is not None:
+                    sequences[current_id] = "".join(current_sequence)
+                
+                current_id = line[1:]
+                current_sequence = []
+            
+            else:
+                current_sequence.append(line)
+        
+        if current_id is not None:
+            sequences[current_id] = "".join(current_sequence)
+            
+    return sequences
 
 def read_lines(filename="input.txt"):
     path = _get_path(filename)
