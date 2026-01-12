98e39ac0c4c79029653f9dd10dc8e2e7c2c43c2d
João Vitor Guedes <jvfernandesg4@outlook.com>
2026-01-12 20:30:50 -0300

    feat(iprb): add solution for Mendel's First Law

---
+++ problems/007-IPRB-mendels-first-law/solution.py
@@ -0,0 +1,20 @@
+from utils.utils import read_input
+import random
+
+from utils.utils import read_input
+
+def solve():
+    data = read_input()
+    k, m, n = map(int, data.split())
+    t = k + m + n
+    
+    p_recessivo = (
+        (n * (n - 1)) + 
+        (n * m) + 
+        (m * (m - 1) * 0.25)
+    ) / (t * (t - 1))
+    
+    return f"{1 - p_recessivo:.5f}"
+
+if __name__ == "__main__":
+    print(solve())
