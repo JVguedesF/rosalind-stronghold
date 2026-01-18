9f683bc2a55bd9e1dd9bbe8700608b9dfa101f37
João Vitor Guedes <jvfernandesg4@outlook.com>
2026-01-18 16:38:26 -0300

    feat(alg): add solution for enumerating signed permutations

---
+++ problems/029-SIGN-enumerating-oriented-gene-orderings/solution.py
@@ -0,0 +1,21 @@
+import itertools
+from utils.utils import read_input
+
+def solve():
+    n = int(read_input())
+    
+    nums = list(range(1, n + 1))
+    signed_permutations = []
+
+    for perm in itertools.permutations(nums):
+        for signs in itertools.product([-1, 1], repeat=n):
+            signed_permutations.append([p * s for p, s in zip(perm, signs)])
+    
+    output = [str(len(signed_permutations))]
+    for p in signed_permutations:
+        output.append(" ".join(map(str, p)))
+        
+    return "\n".join(output)
+
+if __name__ == "__main__":
+    print(solve())
