ce0253577cdb1782ace89af1966aa43d1a09e327
João Vitor Guedes <jvfernandesg4@outlook.com>
2026-01-22 21:02:30 -0300

    refactor: simplify RNA transcription logic

--- problems/002-RNA-transcribing-dna/solution.py
+++ problems/002-RNA-transcribing-dna/solution.py
@@ -1,21 +1,8 @@
 from utils.io import read_input
 
-#! Best solution
-"""
 def solve():
     s = read_input()
     return s.replace('T', 'U')
-"""
-
-def solve():
-  data = read_input()
-  chars = []
-  for char in data:
-      if char == "T":
-          chars.append("U")
-      else:
-          chars.append(char)
-  return "".join(chars)
 
 if __name__ == "__main__":
   print(solve())
